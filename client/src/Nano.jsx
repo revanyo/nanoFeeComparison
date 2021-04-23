4f047570679152444e322f9a8af843ef06955ec1
Ryan Evanyo <ryan.evanyo@gmail.com>
2021-04-23 13:26:31 -0600

    Added Nano reps

--- client/src/Nano.jsx
+++ client/src/Nano.jsx
@@ -3,17 +3,23 @@ import axios from "axios";
 
 function Nano(props) {
   const [price, setPrice] = useState(null);
-  const [btcPrice, setBtcPrice] = useState(null);
+  const [reps, setReps] = useState(null);
 
   useEffect(() => {
     axios.get("https://proxy.nanos.cc/proxy/?action=price").then((res) => {
       setPrice(res.data.quotes.USD.price);
     });
+    axios
+      .get("https://proxy.nanos.cc/proxy/?action=representatives_online")
+      .then((res) => {
+        setReps(res.data.representatives.length);
+      });
   }, []);
-  console.log(btcPrice);
+
   return (
     <div>
       <p>Nano Price {price}</p>
+      <p>Nano Reps {reps}</p>
     </div>
   );
 }
